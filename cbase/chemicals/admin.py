ef58233dd93fa43db0ee090fd5f580f10ce4537d
Abid Hasan <abidhasan@gmail.com>
2020-02-16 12:15:43 -0700

    Create Models for Chemical App (#3)
    * Add initial chemicals migration
    
    * Add models to admin panel
    
    * Remove system file from VCS

--- cbase/chemicals/admin.py
+++ cbase/chemicals/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Premesis)
+admin.site.register(DepartmentRole)
+admin.site.register(Department)
+admin.site.register(UserRole)
+admin.site.register(Shelf)
+admin.site.register(User)
+admin.site.register(ChemicalBox)
+admin.site.register(Supplier)
+admin.site.register(Chemical)
+admin.site.register(Bottle)
